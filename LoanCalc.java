f330fc640e8fb6d5a8df6d89cfef0a333749535b
RuthSteinberg <ruth.steinberg@post.runi.ac.il>
2024-01-03 20:45:03 +0200

    upload

--- LoanCalc.java
+++ LoanCalc.java
@@ -38,9 +38,26 @@ public class LoanCalc {
 	* the number of periods (n), and epsilon, a tolerance level.
 	*/
 	// Side effect: modifies the class variable iterationCounter.
-    public static double bruteForceSolver(double loan, double rate, int n, double epsilon) {  
-    	// Replace the following statement with your code
-    	return 0;
+    public static double bruteForceSolver(double loan, double rate, int n, double epsilon) { 
+		double balance=loan; 
+		double periodical=loan/(double)n;
+		double increment = 0.1;
+		double frate=rate*0.01;
+		iterationCounter=0;
+		double temploan=loan;
+		//double frate=rate*0.01; //rate from percentage to number
+    	while(balance>epsilon)
+		{
+			periodical=periodical+increment;// 1000
+			balance=loan;
+			iterationCounter=iterationCounter+1;
+			for(int i=0; i<n; i++)
+			{
+				temploan=balance;
+				balance=(temploan-periodical)*(1+frate); 	
+			}
+		}
+		return periodical;	
     }
     
     /**
@@ -50,9 +67,25 @@ public class LoanCalc {
 	* the number of periods (n), and epsilon, a tolerance level.
 	*/
 	// Side effect: modifies the class variable iterationCounter.
-    public static double bisectionSolver(double loan, double rate, int n, double epsilon) {  
-    	// Replace the following statement with your code
-    	return 0;
+    public static double bisectionSolver(double loan, double rate, int n, double epsilon) {
+		double l=loan/(double)n;
+		double h=loan;
+		double mid=(l+h)/2;
+		iterationCounter=0;
+		while((h-l)>epsilon)
+		{
+			iterationCounter=iterationCounter+1;
+			if(endBalance(loan, rate,n,mid)*endBalance(loan, rate,n,l)>0)
+			{
+				l=mid;
+			}
+			else
+			{
+				h=mid;	
+			}
+			mid=(l+h)/2;
+		}   
+    	return mid;
     }
 	
 	/**
@@ -60,7 +93,15 @@ public class LoanCalc {
 	* interest rate (as a percentage), the number of periods (n), and the periodical payment.
 	*/
 	private static double endBalance(double loan, double rate, int n, double payment) {
-		// Replace the following statement with your code
-    	return 0;
-	}
+		double temploan=loan;
+		double balance=loan;
+		double frate=rate*0.01;
+		for(int i=0; i<n; i++)
+			{
+				temploan=balance;
+				balance=(temploan-payment)*(1+frate); 	
+			}
+			return balance;
+		}
+		
 }
